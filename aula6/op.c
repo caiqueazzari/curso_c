20e1479118ff9499c427f4d60c1269bc1729f646
John Doe <64171008+caiqueazzari@users.noreply.github.com>
2021-09-28 14:48:57 +0000

    aula6

---
+++ aula6/op.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <limits.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+/*
+ * = atribuição
+ * + - unarios
+ * + - binarios (adicao e subtracao)
+ * * / (multiplicacao e divisao)
+ * % (resto da divisao)
+ * ++ (incremento)
+ * -- (decremento)
+*/
+
+int main(void) {
+  int a;
+
+  a = (4 + 2) * 3;
+
+  printf("%d\n", a--);
+ 
+  printf("%d\n", a);
+
+  return 0;
+}
